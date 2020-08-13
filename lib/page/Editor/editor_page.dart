12684909e19970658641dadc738922463edef8d8
Min Than Htut <fellinglove8@gmail.com>
2020-08-13 18:13:20 +0630

    fix : editor & add item desgin

--- lib/page/Editor/editor_page.dart
+++ lib/page/Editor/editor_page.dart
@@ -1,3 +1,4 @@
+import 'package:ant_icons/ant_icons.dart';
 import 'package:flutter/material.dart';
 
 class EditorPage extends StatefulWidget {
@@ -13,14 +14,75 @@ class _EditorPageState extends State<EditorPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Editor"),
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(AntIcons.bars),
+          )
+        ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            _videoview(),
-            _subtitlelist(),
-          ],
-        ),
+      body: Column(
+        children: <Widget>[
+          _videoview(),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                  _subtitlelist(),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -35,6 +97,48 @@ class _EditorPageState extends State<EditorPage> {
   }
 
   Widget _subtitlelist() {
-    return Text("data");
+    return Row(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Text("0:0:00:00"),
+              Text("0:0:01:00"),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "ABCD abcd",
+                  maxLines: 2,
+                  style: TextStyle(fontSize: 15),
+                ),
+                Text(
+                  "Deaflut",
+                  style: TextStyle(fontSize: 12),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.all(8.0),
+          height: 25,
+          width: 25,
+          color: Colors.green,
+          child: Icon(
+            AntIcons.info_outline,
+            color: Colors.white,
+          ),
+        )
+      ],
+    );
   }
 }
--- lib/page/Subtitle/subtitle_page.dart
+++ lib/page/Subtitle/subtitle_page.dart
@@ -1,7 +1,5 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_subtitle_editor/page/Editor/editor_page.dart';
 import 'package:flutter_subtitle_editor/widget/widget_subtile_file_item.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -15,7 +13,6 @@ class SubtitlePage extends StatefulWidget {
 class _SubtitlePageState extends State<SubtitlePage> {
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     getPermission();
   }
@@ -30,56 +27,6 @@ class _SubtitlePageState extends State<SubtitlePage> {
 
   @override
   Widget build(BuildContext context) {
-    // Directory dir = Directory('/storage/emulated/0/');
-    // String datapath = dir.toString();
-    // print(datapath);
-    // List<FileSystemEntity> _files;
-    // List<FileSystemEntity> _ass = [];
-    // List<FileSystemEntity> _srt = [];
-    // _files = dir.listSync(recursive: true, followLinks: false);
-    // for (FileSystemEntity entity in _files) {
-    //   String path = entity.path;
-    //   if (path.endsWith('.ass')) _ass.add(entity);
-    //   if (path.endsWith('.srt')) _srt.add(entity);
-    // }
-    // print(_ass);
-    // print(_ass.length);
-
-    // Widget _assdata() {
-    //   return Column(
-    //     children: [
-    //       Container(
-    //         width: double.infinity,
-    //         color: Colors.grey,
-    //         child: Padding(
-    //           padding: const EdgeInsets.all(8.0),
-    //           child: Text(
-    //             "ASS",
-    //             style: TextStyle(color: Colors.white),
-    //           ),
-    //         ),
-    //       ),
-    //       FutureBuilder(
-    //           future: assfilelist(),
-    //           builder: (BuildContext context, AsyncSnapshot snapshot) {
-    //             if (snapshot.connectionState == ConnectionState.waiting) {
-    //               return new Text('Data is loading...');
-    //             } else {
-    //               List<FileSystemEntity> srt = snapshot.data;
-    //               // print("ass : ${srt.length}");
-    //               // return Text("data");
-    //               return Column(
-    //                   children: srt
-    //                       .map((e) => SubFileItem(
-    //                             file: e,
-    //                           ))
-    //                       .toList());
-    //             }
-    //           }),
-    //     ],
-    //   );
-    // }
-
     Widget _filddata() {
       return Column(
         children: [
@@ -102,6 +49,7 @@ class _SubtitlePageState extends State<SubtitlePage> {
                     if (path.endsWith('.srt')) srt.add(entity);
                     if (path.endsWith('.ass')) ass.add(entity);
                   }
+
                   return Column(
                     children: <Widget>[
                       Container(
@@ -153,41 +101,11 @@ class _SubtitlePageState extends State<SubtitlePage> {
     );
   }
 
-  // Future<List<FileSystemEntity>> srtfilelist() async {
-  //   Directory dir = Directory('/storage/emulated/0/');
-  //   var filesList = new List<FileSystemEntity>();
-  //   var srt = new List<FileSystemEntity>();
-  //   // filesList = await FilesInDirectory().getFilesFromDir();
-  //   filesList = dir.listSync(recursive: true, followLinks: false);
-  //   for (FileSystemEntity entity in filesList) {
-  //     String path = entity.path;
-  //     if (path.endsWith('.srt')) srt.add(entity);
-  //   }
-  //   await new Future.delayed(new Duration(milliseconds: 500));
-  //   print(srt.length);
-  //   return srt;
-  // }
-
-  // Future<List<FileSystemEntity>> assfilelist() async {
-  //   Directory dir = Directory('/storage/emulated/0/');
-  //   var filesList = new List<FileSystemEntity>();
-  //   var ass = new List<FileSystemEntity>();
-  //   // filesList = await FilesInDirectory().getFilesFromDir();
-  //   filesList = dir.listSync(recursive: true, followLinks: false);
-  //   for (FileSystemEntity entity in filesList) {
-  //     String path = entity.path;
-  //     if (path.endsWith('.ass')) ass.add(entity);
-  //   }
-  //   await new Future.delayed(new Duration(milliseconds: 500));
-  //   print(ass.length);
-  //   return ass;
-  // }
-
   Future<List<FileSystemEntity>> filelist() async {
     Directory dir = Directory('/storage/emulated/0/');
     var filesList = new List<FileSystemEntity>();
     filesList = dir.listSync(recursive: true, followLinks: false);
-    await new Future.delayed(new Duration(milliseconds: 500));
+    await new Future.delayed(new Duration(milliseconds: 1000));
     return filesList;
   }
 }
